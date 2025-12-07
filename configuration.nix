e74108f3c15fe57f0e674da1f1e5106710e29db1
N <revanfds@gmail.com>
2025-12-07 17:53:01 -0300

    nix configuration

--- configuration.nix
+++ configuration.nix
@@ -79,6 +79,18 @@ environment.etc."zsh/p10k".source =
   "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
 
 
+
+  # Flatpak
+  services.flatpak.enable = true;
+    systemd.services.flatpak-repo = {
+      wantedBy = [ "multi-user.target" ];
+      path = [ pkgs.flatpak ];
+      script = ''
+        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+      '';
+    };
+
+
   # Steam
   programs.steam = {
     enable = true;
